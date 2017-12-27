de821ebda699e9043317134a06fc8bb8f6d680fc
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2017-12-27 19:02:09 +0100

    Removed sudo usage to build image, instead build as unprivileged user
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- build_image.sh
+++ build_image.sh
@@ -1,6 +1,6 @@
-sudo docker build  -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl -t lpp/teamcity_agent_osx .
-sudo docker run --privileged --net=host -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl --name=tcosx -it lpp/teamcity_agent_osx
-sudo docker stop tcosx
-sudo docker commit tcosx lpp/teamcity_agent_osx
-sudo docker rm tcosx
+docker build  -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl -t lpp/teamcity_agent_osx .
+docker run --privileged --net=host -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl --name=tcosx -it lpp/teamcity_agent_osx
+docker stop tcosx
+docker commit tcosx lpp/teamcity_agent_osx
+docker rm tcosx
 
