59b16b979e2d9c3479d56603b570d0059ac6b4c8
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2018-01-26 20:27:05 +0100

    removed deadlock during image build
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- build_image.sh
+++ build_image.sh
@@ -1,5 +1,6 @@
 docker build  -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl -t lpp/teamcity_agent_osx .
-docker run --privileged --net=host -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl --name=tcosx -it lpp/teamcity_agent_osx
+docker run --privileged --net=host -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl --name=tcosx -it lpp/teamcity_agent_osx &
+sleep 300
 docker stop tcosx
 docker commit tcosx lpp/teamcity_agent_osx
 docker rm tcosx
