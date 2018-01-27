88b734c30397bb263236481035f218de3d595055
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2018-01-27 11:39:32 +0100

    deadlock fix try#2
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- build_image.sh
+++ build_image.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker build  -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl -t lpp/teamcity_agent_osx .
 docker run --privileged --net=host -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl --name=tcosx -it lpp/teamcity_agent_osx &
 sleep 300
